4343ef3991c6059ed8b1e686261cce59cc940a6b
Maria <itimasha@gmail.com>
2019-06-14 00:58:10 +0300

    task2_const

--- js/stat.js
+++ js/stat.js
@@ -17,10 +17,14 @@ window.renderStatistics = function (ctx, names, times) {
 
   var PADDING = {
     LEFT: 150,
-    TOP: 30
+    TOP: 30,
+    FOR_NAMES: 250,
+    FOR_TIMES: 220,
+    FOR_COLUMNS: 230
   };
 
   var COLUMN_BETWEEN = BAR_CHART.WIDTH + BAR_CHART.DISTANCE;
+  var FONT_SIZE = 16;
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(WINDOW.X + WINDOW.SHADOW, WINDOW.Y + WINDOW.SHADOW, WINDOW.WIDTH, WINDOW.HEIGHT);
@@ -29,9 +33,9 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillRect(WINDOW.X, WINDOW.Y, WINDOW.WIDTH, WINDOW.HEIGHT);
 
   ctx.fillStyle = 'black';
-  ctx.font = '16px PT Mono';
+  ctx.font = FONT_SIZE + 'px PT Mono';
   ctx.fillText('Ура вы победили!', PADDING.LEFT, PADDING.TOP);
-  ctx.fillText('Список результатов:', PADDING.LEFT, 50);
+  ctx.fillText('Список результатов:', PADDING.LEFT, PADDING.TOP + FONT_SIZE);
 
   var timeMax = Math.max.apply(null, times);
 
@@ -39,9 +43,9 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + (Math.random() * 0.9 + 0.1) + ')';
     // opacity should not be too small
     var height = BAR_CHART.HEIGHT * times[i] / timeMax;
-    ctx.fillRect(PADDING.LEFT + COLUMN_BETWEEN * i, 230 - height, BAR_CHART.WIDTH, height);
+    ctx.fillRect(PADDING.LEFT + COLUMN_BETWEEN * i, PADDING.FOR_COLUMNS - height, BAR_CHART.WIDTH, height);
     ctx.fillStyle = 'black';
-    ctx.fillText(names[i], PADDING.LEFT + COLUMN_BETWEEN * i, 250);
-    ctx.fillText(Math.round(times[i]), PADDING.LEFT + COLUMN_BETWEEN * i, 220 - height);
+    ctx.fillText(names[i], PADDING.LEFT + COLUMN_BETWEEN * i, PADDING.FOR_NAMES);
+    ctx.fillText(Math.round(times[i]), PADDING.LEFT + COLUMN_BETWEEN * i, PADDING.FOR_TIMES - height);
   }
 };
