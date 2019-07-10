02de57d1b3970b5dcad226471696d56e43ad7157
Maria <itimasha@gmail.com>
2019-07-11 00:36:15 +0300

    jsonp

---
+++ js/backend.js
@@ -0,0 +1,56 @@
+'use strict';
+
+(function () {
+  var URL = 'https://js.dump.academy/code-and-magick';
+  var URL_DATA = 'https://js.dump.academy/code-and-magick/data';
+
+  window.backend = {
+
+    save: function (data, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+      xhr.timeout = 10000; // 10s
+
+
+      xhr.open('POST', URL);
+      xhr.send(data);
+    },
+
+    load: function (onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      xhr.open('GET', URL_DATA);
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        }});
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+      xhr.timeout = 10000; // 10s
+
+      xhr.send();
+    },
+  };
+
+})();
+
--- js/setup.js
+++ js/setup.js
@@ -2,8 +2,8 @@
 
 (function () {
   // popup generating
-  var NAMES_OPTIONS = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var SURNAME_OPTIONS = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  // var NAMES_OPTIONS = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  // var SURNAME_OPTIONS = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
   var COLOR_OPTIONS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var EYES_OPTIONS = ['black', 'red', 'blue', 'yellow', 'green'];
   var FIREBALL_OPTIONS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
@@ -21,42 +21,75 @@
     return array[rand];
   };
 
-  var generateWizards = function (count) {
-    var wizards = [];
-    for (var i = 0; i < count; i++) {
-      wizards[i] = {
-        name: getRandomOfArray(NAMES_OPTIONS) + ' ' + getRandomOfArray(SURNAME_OPTIONS),
-        coatColor: getRandomOfArray(COLOR_OPTIONS),
-        eyesColor: getRandomOfArray(EYES_OPTIONS)
-      };
-    }
-    return wizards;
-  };
+  // var generateWizards = function (count) {
+  //   var wizards = [];
+  //   for (var i = 0; i < count; i++) {
+  //     wizards[i] = {
+  //       name: getRandomOfArray(NAMES_OPTIONS) + ' ' + getRandomOfArray(SURNAME_OPTIONS),
+  //       coatColor: getRandomOfArray(COLOR_OPTIONS),
+  //       eyesColor: getRandomOfArray(EYES_OPTIONS)
+  //     };
+  //   }
+  //   return wizards;
+  // };
 
   var generateWizardNodes = function (wizardsData) {
     var wizardElements = [];
     var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-    for (var j = 0; j < wizardsData.length; j++) {
+    for (var j = 0; j < WIZARDS_COUNT; j++) {
       wizardElements[j] = similarWizardTemplate.cloneNode(true);
       wizardElements[j].querySelector('.setup-similar-label').textContent = wizardsData[j].name;
-      wizardElements[j].querySelector('.wizard-coat').style.fill = wizardsData[j].coatColor;
-      wizardElements[j].querySelector('.wizard-eyes').style.fill = wizardsData[j].eyesColor;
+      wizardElements[j].querySelector('.wizard-coat').style.fill = wizardsData[j].colorCoat;
+      wizardElements[j].querySelector('.wizard-eyes').style.fill = wizardsData[j].colorEyes;
     }
     return wizardElements;
   };
 
-  var renderWizards = function () {
+  var renderWizards = function (wizards) {
     var similarListElement = document.querySelector('.setup-similar-list');
     var fragment = document.createDocumentFragment();
-    var wizardNodes = generateWizardNodes(generateWizards(WIZARDS_COUNT));
-    for (var i = 0; i < wizardNodes.length; i++) {
+    var wizardNodes = generateWizardNodes(wizards);
+    for (var i = 0; i < WIZARDS_COUNT; i++) {
       fragment.appendChild(wizardNodes[i]);
     }
     similarListElement.appendChild(fragment);
   };
 
   showUserDialog();
-  renderWizards();
+  // renderWizards();
+
+  // new task
+  var form = setup.querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    window.backend.upload(new FormData(form), function (response) {
+      setup.classList.add('hidden');
+    });
+    evt.preventDefault();
+  });
+
+  var onDataLoad = function (wizards) {
+    renderWizards(wizards);
+    setup.querySelector('.setup-similar').classList.remove('hidden');
+  };
+
+  var onDataError = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(onDataLoad, onDataError);
+
+  // window.backend.load(function (wizards) {
+
+  // });
+
 
   // change color of coat
   var wizardCoatColor = setup.querySelector('.wizard-coat');
